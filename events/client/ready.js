c71696e8eb3a41c6060f0c631e80b29a4d0048c7
ahmed0542853710-cloud <ahmed0542853710@gmail.com>
2026-08-14 15:11:04 +0100

    Add files via upload

---
+++ events/client/ready.js
@@ -0,0 +1,10 @@
+const {Events} = require("discord.js")
+
+
+module.exports = {
+    name : Events.ClientReady,
+    once : true,
+    async execute(client) {
+        console.log(`logged in as: ${client.user.tag}`)
+    }
+}
---
+++ handlers/events.js
@@ -0,0 +1,28 @@
+const { readdirSync } = require('node:fs');
+const ascii = require('ascii-table');
+const table = new ascii('Events').setJustify();
+
+module.exports = (client) => {
+    readdirSync('./events').forEach((folder) => {
+        const eventFiles = readdirSync(`./events/${folder}`).filter((file) => 
+            file.endsWith('.js')
+        );
+
+        for (const file of eventFiles) {
+            const event = require(`../events/${folder}/${file}`);
+            
+            if (event && event.name) {
+                if (event.once) {
+                    client.once(event.name, (...args) => event.execute(...args, client));
+                } else {
+                    client.on(event.name, (...args) => event.execute(...args, client));
+                }
+                table.addRow(event.name, "🟢 Working");
+            } else {
+                table.addRow(file, "🔴 Not Working");
+            }
+        }
+    });
+    
+    console.log(table.toString());
+};
