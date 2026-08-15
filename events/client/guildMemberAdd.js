ba4361f6cacece1eed20d659a40ba51754508cec
ahmed0542853710-cloud <ahmed0542853710@gmail.com>
2026-08-15 14:53:42 +0100

    Add files via upload

--- events/client/guildMemberAdd.js
+++ events/client/guildMemberAdd.js
@@ -3,7 +3,6 @@ const { EmbedBuilder } = require("discord.js");
 module.exports = {
     name: "guildMemberAdd",
     async execute(member) {
-        // ID روم الترحيب الخاص بسيرفرك
         const welcomeChannelId = "1395447275982946374"; 
         
         try {
@@ -12,12 +11,14 @@ module.exports = {
 
             if (!channel) return console.log("❌ لم يتم العثور على روم الترحيب!");
 
-            // تصميم البطاقة الفخمة
+            // رابط صورة العضو بشكل مضمون وواضح
+            const userAvatar = member.user.displayAvatarURL({ extension: 'png', forceStatic: false, size: 512 });
+
             const welcomeEmbed = new EmbedBuilder()
-                .setColor("2b2d31")
+                .setColor("#2b2d31")
                 .setAuthor({ 
                     name: `WELCOME TO ${member.guild.name.toUpperCase()}`, 
-                    iconURL: member.guild.iconURL({ dynamic: true }) 
+                    iconURL: member.guild.iconURL({ extension: 'png' }) || undefined
                 })
                 .setTitle(`👑 أهلاً بك في العائلة!`)
                 .setDescription(
@@ -26,11 +27,11 @@ module.exports = {
                     `> 📜 **نتمنى لك قضاء وقت ممتع والمشاركة معنا!**\n` +
                     `> 💎 **أنت العضو رقم:** \`#${member.guild.memberCount}\``
                 )
-                .setThumbnail(member.user.displayAvatarURL({ dynamic: true, size: 512 }))
+                .setThumbnail(userAvatar)
                 .setImage("https://cdn.discordapp.com/attachments/1395743517875372062/1534945621327478865/Gemini_Generated_Image_fueabcfueabcfuea.png?ex=6a812dc2&is=6a7fdc42&hm=14a49dd797d897a0efb9decfdb0bf04e4930910bf89b38322d4d1460693a7367&")
                 .setFooter({ 
                     text: `${member.user.tag} • WAGNER SYSTEM`, 
-                    iconURL: member.user.displayAvatarURL({ dynamic: true }) 
+                    iconURL: userAvatar // 👈 استخدام الرابط المباشر
                 })
                 .setTimestamp();
 
